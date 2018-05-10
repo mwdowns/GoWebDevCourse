d182fd724d6861a8691411c4078412d18d4fd63e
mwdowns <mwdowns@gmail.com>
2018-05-10 07:23:23 -0400

    more work with templates

--- templates/main.go
+++ templates/main.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"strings"
+	"text/template"
 )
 
 func main() {
-	name := os.Args[1]
+	// name := os.Args[1]
 
-	tpl := fmt.Sprint(`
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-	<meta charset="UTF-8">
-	<title>Hello World!</title>
-	</head>
-	<body>
-	<h1>` + name + `</h1>
-	</body>
-	</html>
-	`)
-	nf, err := os.Create("index.html")
+	// tpl := fmt.Sprint(`
+	// <!DOCTYPE html>
+	// <html lang="en">
+	// <head>
+	// <meta charset="UTF-8">
+	// <title>Hello World!</title>
+	// </head>
+	// <body>
+	// 	<h1>` + name + `</h1>
+	// </body>
+	// </html>
+	// `)
+	// nf, err := os.Create("index.html")
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// }
+	// defer nf.Close()
+
+	// io.Copy(nf, strings.NewReader(tpl))
+
+	tpl, err := template.ParseFiles("tpl.gohtml")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
-	defer nf.Close()
 
-	io.Copy(nf, strings.NewReader(tpl))
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
