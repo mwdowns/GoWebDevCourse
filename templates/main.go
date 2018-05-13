bf8dea3dc1dcd496ba705b5d784272d98c37f867
mwdowns <mwdowns@gmail.com>
2018-05-13 18:16:29 -0400

    more practice with templates and passing in data

--- templates/main.go
+++ templates/main.go
@@ -6,6 +6,27 @@ import (
 	"text/template"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+type being struct {
+	Name string
+	Type string
+}
+
+type favorites struct {
+	FavoriteFood   string
+	FavoritePerson string
+}
+
+type items struct {
+	Beings    []being
+	Favorites []favorites
+}
+
 func main() {
 	// name := os.Args[1]
 
@@ -29,17 +50,47 @@ func main() {
 
 	// io.Copy(nf, strings.NewReader(tpl))
 
-	tpl, err := template.ParseFiles("tpl.gohtml")
-	if err != nil {
-		fmt.Println("Error: ", err)
+	// tpl, err := template.ParseGlob("templates/*.gohtml")
+	// errorHandler(err)
+
+	people := []string{"Jeri", "Matt", "Doodins", "Stinkins"}
+	people2 := []being{
+		{Name: "Jeri", Type: "person"},
+		{Name: "Matt", Type: "person"},
+		{Name: "Doodins", Type: "cat"},
+		{Name: "Stinkins", Type: "cat"},
+	}
+	favorites := []favorites{
+		{FavoriteFood: "Vegan/NonDairy", FavoritePerson: "Doodins"},
+		{FavoriteFood: "Goldfish", FavoritePerson: "Stikins"},
+		{FavoriteFood: "Milkins", FavoritePerson: "Jeri"},
+		{FavoriteFood: "Tunins", FavoritePerson: "Matt"},
+	}
+	stuff := items{
+		Beings:    people2,
+		Favorites: favorites,
 	}
 
-	nf, err := os.Create("index.html")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
+	nf1, err := os.Create("index.html")
+	errorHandler(err)
 
-	err = tpl.Execute(nf, nil)
+	nf2, err := os.Create("about.html")
+	errorHandler(err)
+
+	nf3, err := os.Create("about2.html")
+	errorHandler(err)
+
+	err = tpl.ExecuteTemplate(nf1, "tpl.gohtml", people)
+	errorHandler(err)
+
+	err = tpl.ExecuteTemplate(nf2, "tpl2.gohtml", people2)
+	errorHandler(err)
+
+	err = tpl.ExecuteTemplate(nf3, "tpl3.gohtml", stuff)
+	errorHandler(err)
+}
+
+func errorHandler(err error) {
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
