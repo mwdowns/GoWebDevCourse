fb4ddb4c7258faa2cc75eb13144e86d3896f8bce
mwdowns <mwdowns@gmail.com>
2018-05-15 08:53:58 -0400

    refactor template creation logic a bit

--- templates/main.go
+++ templates/main.go
@@ -12,9 +12,14 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
+type templates struct {
+	pageName     string
+	templateName string
+}
+
 type being struct {
-	Name string
-	Type string
+	Name   string
+	Animal string
 }
 
 type favorites struct {
@@ -53,41 +58,58 @@ func main() {
 	// tpl, err := template.ParseGlob("templates/*.gohtml")
 	// errorHandler(err)
 
-	people := []string{"Jeri", "Matt", "Doodins", "Stinkins"}
-	people2 := []being{
-		{Name: "Jeri", Type: "person"},
-		{Name: "Matt", Type: "person"},
-		{Name: "Doodins", Type: "cat"},
-		{Name: "Stinkins", Type: "cat"},
+	// people := []string{"Jeri", "Matt", "Doodins", "Stinkins"}
+	pages := []templates{
+		{pageName: "index.html", templateName: "tpl.gohtml"},
+		{pageName: "about.html", templateName: "tpl2.gohtml"},
+		{pageName: "about2.html", templateName: "tpl3.gohtml"},
+	}
+	beings := []being{
+		{Name: "Jeri", Animal: "person"},
+		{Name: "Matt", Animal: "person"},
+		{Name: "Doodins", Animal: "cat"},
+		{Name: "Stinkins", Animal: "cat"},
+		{Name: "Rovi", Animal: "dog"},
+		{Name: "Danny", Animal: "good boy"},
 	}
 	favorites := []favorites{
 		{FavoriteFood: "Vegan/NonDairy", FavoritePerson: "Doodins"},
 		{FavoriteFood: "Goldfish", FavoritePerson: "Stikins"},
 		{FavoriteFood: "Milkins", FavoritePerson: "Jeri"},
 		{FavoriteFood: "Tunins", FavoritePerson: "Matt"},
+		{FavoriteFood: "Turkey Neck", FavoritePerson: "Jeri"},
+		{FavoriteFood: "Buscuit", FavoritePerson: "Matt"},
 	}
 	stuff := items{
-		Beings:    people2,
+		Beings:    beings,
 		Favorites: favorites,
 	}
 
-	nf1, err := os.Create("index.html")
-	errorHandler(err)
+	for _, files := range pages {
+		nf, err := os.Create(files.pageName)
+		errorHandler(err)
 
-	nf2, err := os.Create("about.html")
-	errorHandler(err)
+		err = tpl.ExecuteTemplate(nf, files.templateName, stuff)
+		errorHandler(err)
+	}
 
-	nf3, err := os.Create("about2.html")
-	errorHandler(err)
+	// nf1, err := os.Create("index.html")
+	// errorHandler(err)
 
-	err = tpl.ExecuteTemplate(nf1, "tpl.gohtml", people)
-	errorHandler(err)
+	// nf2, err := os.Create("about.html")
+	// errorHandler(err)
 
-	err = tpl.ExecuteTemplate(nf2, "tpl2.gohtml", people2)
-	errorHandler(err)
+	// nf3, err := os.Create("about2.html")
+	// errorHandler(err)
 
-	err = tpl.ExecuteTemplate(nf3, "tpl3.gohtml", stuff)
-	errorHandler(err)
+	// err = tpl.ExecuteTemplate(nf1, "tpl.gohtml", people)
+	// errorHandler(err)
+
+	// err = tpl.ExecuteTemplate(nf2, "tpl2.gohtml", people2)
+	// errorHandler(err)
+
+	// err = tpl.ExecuteTemplate(nf3, "tpl3.gohtml", stuff)
+	// errorHandler(err)
 }
 
 func errorHandler(err error) {
