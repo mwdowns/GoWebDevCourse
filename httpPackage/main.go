0987caac2703dfeebca00f7b4205aa11ce67aacf
mwdowns <mwdowns@gmail.com>
2018-06-06 10:25:56 -0400

    work with mux

--- httpPackage/main.go
+++ httpPackage/main.go
@@ -6,24 +6,38 @@ import (
 	"net/http"
 )
 
-type hotdog int
-
-func (h hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// this call here to ParseForm() makes the data from the form submit avaialbe
-	// and it is passed to the template below as r.Form
-	err := r.ParseForm()
+func errorHandle(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	w.Header().Set("Matt-key", "yo, dis Matt")
-	switch r.URL.Path {
-	case "/dog":
-		tpl.ExecuteTemplate(w, "dog.gohtml", r.Form)
-	case "/cat":
-		tpl.ExecuteTemplate(w, "cat.gohtml", r.Form)
-	default:
-		tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
-	}
+}
+
+type hotdog int
+
+func (d hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// this call here to ParseForm() makes the data from the form submit avaialbe
+	// and it is passed to the template below as r.Form
+	err := r.ParseForm()
+	errorHandle(err)
+	w.Header().Set("Dog-Key", "yo, dis dog")
+	// switch r.URL.Path {
+	// case "/dog":
+	// 	tpl.ExecuteTemplate(w, "dog.gohtml", r.Form)
+	// case "/cat":
+	// 	tpl.ExecuteTemplate(w, "cat.gohtml", r.Form)
+	// default:
+	// 	tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+	// }
+	tpl.ExecuteTemplate(w, "dog.gohtml", r.Form)
+}
+
+type hotcat int
+
+func (c hotcat) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	errorHandle(err)
+	w.Header().Set("Cat-Key", "yo, dis cat")
+	tpl.ExecuteTemplate(w, "cat.gohtml", r.Form)
 }
 
 var tpl *template.Template
@@ -34,5 +48,10 @@ func init() {
 
 func main() {
 	var d hotdog
-	http.ListenAndServe(":8080", d)
+	var c hotcat
+
+	mux := http.NewServeMux()
+	mux.Handle("/dog", d)
+	mux.Handle("/cat", c)
+	http.ListenAndServe(":8080", mux)
 }
