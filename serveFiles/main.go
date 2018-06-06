20ff9a06a048fb3b6cf857bbdb77c9087e13b7bb
mwdowns <mwdowns@gmail.com>
2018-06-06 13:55:56 -0400

    serving files practice

---
+++ serveFiles/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", sloth)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func sloth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	io.WriteString(w, `
+		<img src="/resources/felice.jpg">
+	`)
+}
+
+// func slothPic(w http.ResponseWriter, r *http.Request) {
+// 	f, err := os.Open("felice.jpg")
+// 	if err != nil {
+// 		http.Error(w, "file not found", 404)
+// 		return
+// 	}
+// 	defer f.Close()
+
+// 	io.Copy(w, f)
+// }
