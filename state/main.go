d5bd7d596e0564a9fd4310dc4750c72e623d5122
mwdowns <mwdowns@gmail.com>
2018-06-08 18:58:44 -0400

    more work with states, adds templates, various handle funcs

--- state/main.go
+++ state/main.go
@@ -1,23 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
+// var tpl *template.Template
+
+// type person struct {
+// 	FirstName string
+// 	LastName  string
+// 	Subscribe bool
+// }
+
+// func init() {
+// 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+// }
+
+func errorHandler(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", blah)
+	// http.HandleFunc("/", v)
+	// http.HandleFunc("/", p)
+	http.HandleFunc("/", rf)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func blah(w http.ResponseWriter, r *http.Request) {
-	v := r.FormValue("q")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// func v(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	io.WriteString(w, `
+// 		<form method="post">
+// 		<input type="text" name="q">
+// 		<input type="submit">
+// 		</form><br>
+// 		`+v)
+// }
+
+// func p(w http.ResponseWriter, r *http.Request) {
+// 	f := r.FormValue("first")
+// 	l := r.FormValue("last")
+// 	s := r.FormValue("subscribe") == "on"
+
+// 	err := tpl.ExecuteTemplate(w, "index.gohtml", person{f, l, s})
+// 	errorHandler(err)
+// }
+
+func rf(w http.ResponseWriter, r *http.Request) {
+	var s string
+	fmt.Println(r.Method)
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("file")
+		errorHandler(err)
+		defer f.Close()
+
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr:", err)
+
+		bs, err := ioutil.ReadAll(f)
+		errorHandler(err)
+
+		s = string(bs)
+	}
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	io.WriteString(w, `
-		<form method="post">
-		<input type="text" name="q">
+		<form method="POST" enctype="multipart/form-data">
+		<input type="file" name="file">
 		<input type="submit">
-		</form><br>
-		`+v)
+		</form>
+		<br>`+s)
 }
