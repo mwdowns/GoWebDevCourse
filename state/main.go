06bf7ec479ff969ce41a959cae471a94d41cf437
Matthew Downs <mwdowns@gmail.com>
2018-06-08 20:33:18 -0400

    now i can have all my experiements running at the same time

--- state/main.go
+++ state/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
 
 var tpl *template.Template
 
-// type person struct {
-// 	FirstName string
-// 	LastName  string
-// 	Subscribe bool
-// }
+type person struct {
+	FirstName string
+	LastName  string
+	Subscribe bool
+}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
@@ -27,31 +28,40 @@ func errorHandler(err error) {
 }
 
 func main() {
-	// http.HandleFunc("/", v)
-	// http.HandleFunc("/", p)
-	http.HandleFunc("/", rf)
+	http.HandleFunc("/", i)
+	http.HandleFunc("/v", v)
+	http.HandleFunc("/p", p)
+	http.HandleFunc("/rf", rf)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-// func v(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	io.WriteString(w, `
-// 		<form method="post">
-// 		<input type="text" name="q">
-// 		<input type="submit">
-// 		</form><br>
-// 		`+v)
-// }
+func i(w http.ResponseWriter, r *http.Request) {
+	name := "Matt"
+	err := tpl.ExecuteTemplate(w, "index_i.gohtml", name)
+	errorHandler(err)
+}
 
-// func p(w http.ResponseWriter, r *http.Request) {
-// 	f := r.FormValue("first")
-// 	l := r.FormValue("last")
-// 	s := r.FormValue("subscribe") == "on"
+func v(w http.ResponseWriter, r *http.Request) {
+	value := r.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	io.WriteString(w, `
+		<a href="/">Home</a>
+		<form method="post">
+		<input type="text" name="q">
+		<input type="submit">
+		</form><br>
+		`+value)
+}
 
-// 	err := tpl.ExecuteTemplate(w, "index_p.gohtml", person{f, l, s})
-// 	errorHandler(err)
-// }
+func p(w http.ResponseWriter, r *http.Request) {
+	f := r.FormValue("first")
+	l := r.FormValue("last")
+	s := r.FormValue("subscribe") == "on"
+
+	err := tpl.ExecuteTemplate(w, "index_p.gohtml", person{f, l, s})
+	errorHandler(err)
+}
 
 func rf(w http.ResponseWriter, r *http.Request) {
 	var s string
